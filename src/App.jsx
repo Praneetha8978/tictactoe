824e6471154655a554c60a3b5596a523582d9d8e
praneetha devi mudunuri <20b01a12b3@svecw.edu.in>
2023-01-13 10:55:21 +0530

    my tic tac toe game

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const App = () => {
+  return(
+    <>
+      <h1>
+        Welcome Im praneetha......
+      </h1>
+      <h2>
+        Im back with my new project......... 
+      </h2>
+    </>
+  )
+}
+export default App;
